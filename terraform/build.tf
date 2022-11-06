bd2943396355e55e67b68ac52315605bf4724625
Craig Thacker <craigthackerx@gmail.com>
2022-11-06 21:34:33 +0000

    update build

--- terraform/build.tf
+++ terraform/build.tf
@@ -104,3 +104,26 @@ module "run_command_lnx" {
 
   script_file = file("${path.cwd}/install-script.sh")
 }
+
+variable "custom_nsg_rules" {
+  default = {
+    "AllowVNetInbound"  = { priority = "110", direction = "Inbound", access = "Allow", source_address_prefix = "VirtualNetwork", destination_address_prefix = "*" },
+  }
+  description = "Rules to be added to nsg"
+}
+
+resource "azurerm_network_security_rule" "allow_rules" {
+  for_each = var.custom_nsg_rules
+
+  name                        = each.key
+  priority                    = each.value.priority
+  direction                   = each.value.direction
+  access                      = each.value.access
+  protocol                    = "*"
+  source_port_range           = "*"
+  destination_port_range      = "*"
+  source_address_prefix       = each.value.source_address_prefix
+  destination_address_prefix  = each.value.destination_address_prefix
+  resource_group_name         = module.rg.rg_name
+  network_security_group_name = module.nsg.nsg_name
+}
