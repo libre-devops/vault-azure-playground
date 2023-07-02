174529e65f5ec429792f482335f2cc4213428945
Craig Thacker <craigthackerx@gmail.com>
2023-07-02 02:28:32 +0100

    update

--- terraform/0_prep/linux-vm.tf
+++ terraform/0_prep/linux-vm.tf
@@ -12,8 +12,50 @@ module "linux_vm" {
   use_simple_image_with_plan = false
   vm_os_simple               = "Ubuntu22.04"
   vm_os_disk_size_gb         = "127"
-  custom_data                = data.template_cloudinit_config.config.rendered
-  user_data                  = base64encode(data.azurerm_client_config.current.tenant_id)
+
+  custom_data = base64encode(<<-EOF
+    #cloud-config
+    package_upgrade: true
+    package_update: true
+
+    packages:
+      - cifs-utils
+      - lsof
+      - gpg
+      - curl
+      - wget
+      - jq
+      - nano
+      - apt-transport-https
+
+    runcmd:
+      - apt-get update
+      - apt-get dist-upgrade
+      - sh -c 'wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg'
+      - sh -c 'echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | tee /etc/apt/sources.list.d/hashicorp.list'
+      - mkdir -p /etc/vault.d
+      - adduser vault
+      - adduser nginx
+      - mkdir -p /etc/nginx
+      - chown -R nginx:nginx /etc/nginx
+      - apt-get update
+      - apt-get install -y vault
+      - chown vault:vault /etc/vault.d/vault.hcl
+      - sh -c 'echo export VAULT_ADDR="http://127.0.0.1:8200" >> /etc/environment'
+      - systemctl daemon-reload
+      - systemctl start vault
+      - systemctl enable vault
+      - STORAGE_ACCOUNT_NAME=${module.sa.sa_name}
+      - STORAGE_ACCOUNT_KEY=$(az storage account keys list --account-name $STORAGE_ACCOUNT_NAME --query "[0].value" --output tsv)
+      - MNT_PATH="/mnt/${module.sa.sa_name}"
+      - SMB_PATH="//${STORAGE_ACCOUNT_NAME}.file.core.windows.net/${azurerm_storage_share.share.name}"
+      - mkdir -p $MNT_PATH
+      - mount -t cifs $SMB_PATH $MNT_PATH -o vers=3.0,username=$STORAGE_ACCOUNT_NAME,password=$STORAGE_ACCOUNT_KEY,serverino,nosharesock,actimeo=30,mfsymlinks
+      - echo "$SMB_PATH $MNT_PATH cifs vers=3.0,username=$STORAGE_ACCOUNT_NAME,password=$STORAGE_ACCOUNT_KEY,serverino,nosharesock,actimeo=30,mfsymlinks 0 0" >> /etc/fstab
+    EOF
+  )
+
+  user_data = base64encode(data.azurerm_client_config.current.tenant_id)
 
   asg_name = "asg-${element(regexall("[a-z]+", element(module.linux_vm.vm_name, 0)), 0)}-${var.short}-${var.loc}-${terraform.workspace}-01" //asg-vmldoeuwdev-ldo-euw-dev-01 - Regex strips all numbers from string
 
---
+++ terraform/0_prep/sa.tf
@@ -0,0 +1,49 @@
+module "sa" {
+  source = "registry.terraform.io/libre-devops/storage-account/azurerm"
+
+  rg_name  = module.rg.rg_name
+  location = module.rg.rg_location
+  tags     = module.rg.rg_tags
+
+  storage_account_name = "st${var.short}${var.loc}${terraform.workspace}${random_string.random.result}01"
+  access_tier          = "Hot"
+  identity_type        = "SystemAssigned"
+
+  storage_account_properties = {
+
+    // Set this block to enable network rules
+    network_rules = {
+      default_action = "Deny"
+      bypass         = ["AzureServices", "Metrics", "Logging"]
+      ip_rules       = []
+      subnet_ids     = [element(values(module.network.subnets_ids), 0)]
+    }
+
+    blob_properties = {
+      versioning_enabled       = false
+      change_feed_enabled      = false
+      default_service_version  = "2020-06-12"
+      last_access_time_enabled = false
+
+      deletion_retention_policies = {
+        days = 10
+      }
+
+      container_delete_retention_policy = {
+        days = 10
+      }
+    }
+
+    routing = {
+      publish_internet_endpoints  = false
+      publish_microsoft_endpoints = true
+      choice                      = "MicrosoftRouting"
+    }
+  }
+}
+
+resource "azurerm_storage_share" "share" {
+  name                 = random_string.random.result
+  storage_account_name = module.sa.sa_name
+  quota                = 50
+}
